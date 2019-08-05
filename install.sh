1daf393b3c911d25b8adeec73495073beccfdb97
Stig Palmquist <stig@stig.io>
2019-08-05 12:57:22 +0200

    use built in sha384sum, tidy up a bit

--- install.sh
+++ install.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
-
-EXPECTED_SIGNATURE="$(curl -sS https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
-fi
-
+set -ex
+function finish {
+    rm composer-setup.php || true
+}
+trap finish EXIT
+curl -sSo composer-setup.php https://getcomposer.org/installer
+curl -sS https://composer.github.io/installer.sha384sum | sha384sum -c -
 php composer-setup.php --quiet --filename=composer --install-dir=/bin
-RESULT=$?
-rm composer-setup.php
-exit $RESULT
+
 
