1bed7a3516ad07c808e8605e9699467aeb0dc3bc
moander <moander@outlook.com>
2020-10-26 18:49:29 +0100

    v1.10.16

---
+++ install_v1.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+EXPECTED_SIGNATURE="$(php -r "echo file_get_contents('https://composer.github.io/installer.sig');")"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet --filename=composer --install-dir=/bin --version=1.10.16
+RESULT=$?
+rm composer-setup.php
+exit $RESULT
+
